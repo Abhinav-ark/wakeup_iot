83e534bdfd35c80e73d2ad66df3546b3df10b656
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-05-18 00:24:14 +0530

    navbar changes

--- frontend/src/app/_components/Navbar.jsx
+++ frontend/src/app/_components/Navbar.jsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import React, { useContext, useEffect } from "react";
 import {ProfileCard} from '@/app/_components';
+import { FaClock } from "react-icons/fa6";
 
 // const crypto = require("crypto");
 
@@ -14,8 +15,9 @@ const Navbar = ({user}) => {
   
   return (
     <div className='shadow-md w-[100vw] h-20 flex flex-row justify-between items-center'>
-      <div>
-        <h1 className='text-2xl font-bold'>Logo</h1>
+      <div className='flex justify-center items-center flex-row pl-10 space-x-2'>
+        <FaClock size={16}/>
+        <h1 className='text-lg font-bold'>WakeUp</h1>
       </div>
       <div className='w-14 h-14 justify-center items-center mx-10'>
         {/* <Image
