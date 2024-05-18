1ded50ad8e61a8c73d64c8c4111634fbdd2fc250
Sowrabh0-0 <sksseervi@gmail.com>
2024-05-18 15:23:39 +0530

    Some changes
    Co-Authored-By: Abhinav Ramakrishnan <76396917+Abhinav-ark@users.noreply.github.com>

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={${inter.className} max-w-[100%] overflow-x-clip}>
+      <body className={`${inter.className} max-w-[100%] overflow-x-clip`}>
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
