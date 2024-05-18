cf375f43bbd2eab9219f2ee08735470074f1a8bb
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-05-19 02:53:53 +0530

    database update

--- backend/schema/data.sql
+++ backend/schema/data.sql
@@ -8,3 +8,6 @@ INSERT INTO alarms (userEmail, alarmTime, alarmDescription) VALUES ('shivajivayi
 INSERT INTO alarms (userEmail, alarmTime, alarmDescription) VALUES ('shivajivayilajilebi@gmail.com', '2021-06-01 12:30:00', 'Go to sleep');
 INSERT INTO alarms (userEmail, alarmTime, alarmDescription) VALUES ('shivajivayilajilebi@gmail.com', '2021-06-01 13:00:00', 'Eat food');
 
+INSERT INTO alarms (userEmail, alarmTime, alarmDescription) VALUES ('abhinavramki2@gmail.com', '2024-05-20 07:00:00', 'Teams Meeting With Swami Ji');
+INSERT INTO alarms (userEmail, alarmTime, alarmDescription) VALUES ('abhinavramki2@gmail.com', '2024-05-21 14:00:00', 'Online Doctor Consultation');
+
